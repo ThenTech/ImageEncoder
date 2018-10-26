7d72f310e96863719968b13ef6ba45fa8889c72e
Wosser1sProductions <wosser1s@hotmail.com>
2018-10-26 17:35:25 +0200

    Re-added extern template def to remove warnings

--- Block.hpp
+++ Block.hpp
@@ -50,6 +50,8 @@ namespace dc {
             static void CreateZigZagLUT(void);
             static constexpr size_t SIZE_LEN_BITS = 4;  ///< The amount of bits to use to represent the bit length of values insode the Block.
     };
+
+    extern template class dc::Block<dc::BlockSize>;
 }
 
 #endif // BLOCK_HPP
